47ab97b8e98f93610c198ecf00e6dc930b0b5a1b
Nebi017 <wedizere123@gmail.com>
2025-01-22 12:24:48 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDatabase = async () => {
   try {
     const connection = await mongoose.connect(
-      "mongodb+srv://nebi:cGySubpjWEm9MweS@nebiy-cluster.wxnm5.mongodb.net/?retryWrites=true&w=majority&appName=nebiy-cluster"
+      "mongodb+srv://nebi:YiPaD3jJqSEetOzK@nebiy-cluster.wxnm5.mongodb.net/?retryWrites=true&w=majority&appName=nebiy-cluster"
     );
     console.log("Database Connected");
   } catch (error) {
